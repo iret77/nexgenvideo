820fa880252cc16e5bf4ca4f06f7224aac810951
Christian Wendler <freediver77@icloud.com>
2026-08-29 12:22:01 +0200

    release: update v1.4.4 pack contract expectations

--- Tests/NexGenEngineTests/MusicvideoPackTests.swift
+++ Tests/NexGenEngineTests/MusicvideoPackTests.swift
@@ -53,8 +53,8 @@ struct MusicvideoPackTests {
     func packSatisfiesContract() {
         let pack: Pack = MusicvideoPack()
         #expect(pack.name == "musicvideo")
-        #expect(pack.version == "0.4.3")
-        #expect(pack.manifest.minAppVersion == "1.4.3")
+        #expect(pack.version == "0.4.4")
+        #expect(pack.manifest.minAppVersion == "1.4.4")
     }
 
     @Test("pack exposes gallery manifest and a starter")
